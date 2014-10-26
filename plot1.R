40678ca5027a6fbada4d7731c97428ed56c104ac
zxmint <zhangxu890826@gmail.com>
2014-10-26 15:08:44 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+library(plyr)
+library(ggplot2)  
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")  
+aggregate.data <- with(NEI, aggregate(Emissions, by = list(year), sum))   
+plot(aggregate.data, type = "o", ylab = expression("Total Emissions, PM"[2.5]),      xlab = "Year", main = "Total Emissions in the United States") 
+polygon(aggregate.data, col = "green", border = "blue")
